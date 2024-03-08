a87c196d0ed374bbbdc8178391ef22f2c6f5cfd4
Tuhin <tuhinsubhraghara29195@gmail.com>
2024-03-08 20:28:50 +0530

    updaye wordfilter

--- modules/wordfilter.py
+++ modules/wordfilter.py
@@ -1,2 +1,10 @@
-def wordfilter(words:list, string:str):
-    return string
+def fillterWord(badWord:list, string:str):
+    for i in badWord:
+        while i in string:
+            inde= string.index(i)
+            count= len(string[inde])-3
+            start= string[inde][:2]
+            end= string[inde][-1]
+            mid= ("*" * count)
+            string[inde] = (start+mid+end)
+    return (" ".join(string))
