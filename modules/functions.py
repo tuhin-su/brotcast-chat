3cabd362a0c17edecc4c633d9ca484719500255d
Tuhin <tuhinsubhraghara29195@gmail.com>
2024-03-09 12:43:00 +0530

    For fixing the upper case lowercase matching

--- modules/functions.py
+++ modules/functions.py
@@ -1,10 +1,16 @@
 def fillterWord(badWord:list, string:str):
-    for i in badWord:
-        while i in string:
-            inde= string.index(i)
-            count= len(string[inde])-3
-            start= string[inde][:2]
-            end= string[inde][-1]
-            mid= ("*" * count)
-            string[inde] = (start+mid+end)
+    string=string.split(" ")
+    for badword in badWord: 
+        for i, word in enumerate(string):
+            if badword.lower() == word.lower():
+                inde= string.index(word)
+                count= len(string[inde])-3
+                start= string[inde][:2]
+                end= string[inde][-1]
+                if count <= 0:
+                    count=count+2
+                    start= string[inde][:1]
+                    end=""
+                mid= ("*" * count)
+                string[inde] = (start+mid+end)
     return (" ".join(string))
